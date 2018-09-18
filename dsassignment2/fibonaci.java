b170513a577fc5b92be6a4901e0125cb3e0d3946
bhrigujai05 <42268718+bhrigujai05@users.noreply.github.com>
2018-09-18 10:45:11 +0530

    dsassignment2

---
+++ dsassignment2/fibonaci.java
@@ -0,0 +1,30 @@
+ import java.util.Scanner;
+ 
+ class fibonaci
+
+{
+public static int fibonacii(int n)
+{
+	if(n==1)
+	{
+		return 0;
+	}
+	else if(n==2)
+	{
+		return 1;
+
+	}
+	else 
+	{
+		return (fibonacii(n-1)+fibonacii(n-2));
+	}
+}
+
+
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	System.out.println(fibonacii(a));
+
+}
+}
