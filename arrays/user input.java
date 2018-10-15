7f9fe6deadd2065a62cf1c6f59636c631135635a
bhrigujai05 <42268718+bhrigujai05@users.noreply.github.com>
2018-10-15 09:53:01 -0400

    array n dynamic array

---
+++ arrays/user input.java
@@ -0,0 +1,29 @@
+public class TakingInput {
+
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("enter number of elements");
+
+        int n=s.nextInt();
+
+        int arr[]=new int[n];
+
+        System.out.println("enter elements");
+
+        for(int i=0;i<n;i++)
+            arr[i]=s.nextInt();
+
+        }
+
+        for(int i: arr){ 
+
+            System.out.println(i);
+
+        }
+
+
+    }
+
+}
